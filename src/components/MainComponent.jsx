825d21d56285c1756627e762543677b36b9fbeed
Andrea Barajas <108833574+AndreBarajas@users.noreply.github.com>
2023-02-26 22:15:37 -0600

    feat: adds react-router-dom to project

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,6 +1,7 @@
 import NewClient from './NewClient';
 import Clients from './Clients';
-import { useState} from 'react'
+import { useState } from 'react'
+import Navbar from './Navbar';
 
 const Prueba = () => { 
    
@@ -13,20 +14,12 @@ const Prueba = () => {
     return (
        
         <div className=' bg-violet-200 h-screen font-display '>
-            <div className='text-5xl font-semibold p-10 pl-80 '>
-                <h1>Bienvenido a Credify</h1>
-            </div>
-            <div className='flex items-center pl-72'>       
-                    <h1 className=' text-3xl font-normal p-8' >Clientes</h1>      
-                <button
-                    onClick={() => handleClickNew(true)}
-                    className='bg-yellow-200 hover:bg-yellow-300 px-3 py-3 rounded '
-                    >Agregar nuevo cliente</button>
-            </div>
-
-            <div className='pl-96 w-screen'>
+            
+                <Navbar />
+        
+           <div className='ml-96 w-screen'>
                 <NewClient stateModal={stateModal} handleClickNew={handleClickNew } />
-                <Clients stateModal={stateModal} />
+                <Clients stateModal={stateModal} handleClickNew={handleClickNew } />
             </div>
         </div>
     )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+
+    return (
+        <div>
+            <div className='bg-slate-50 text-5xl font-semibold p-10 pl-80 rounded '>
+                <h1>Bienvenido a Credify</h1>
+            </div>
+            
+        </div>
+    )
+}; 
+
+export default Navbar; 
